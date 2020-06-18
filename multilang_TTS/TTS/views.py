451f0766ceb03d247827d7a2b3ee612ac1ac0317
NitinSelvam <nitin.sel@gmail.com>
2020-06-18 17:03:28 +0530

    Update views.py

--- multilang_TTS/TTS/views.py
+++ multilang_TTS/TTS/views.py
@@ -15,11 +15,9 @@ def tts(request):
 	text_list = request.query_params.getlist('text_list[]')
 	print("Text list: ",text_list)
 
-	sentence = ' '.join(text_list)
-	print("Full text: ", sentence)
 	lang = request.query_params.get('lang')
 	print("lang: ",lang)
-	if lang is not None and sentence!='':
-		speech(sentence,lang)
+	if lang is not None and text_list!=[]:
+		for sentence in text_list:
+			speech(sentence,lang)
 	return render(request, "translate_speech_to_text.html")
-
